9c3dac64d28e9e39d9907940b0a30038979f5150
Dario Lang <dario.lang@flagbit.de>
2018-10-07 21:41:03 +0200

    [master] add mail.php

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+
+echo "name:" . $name . "!";
+echo "email:" . $email . ".";
+echo "message:" $message;
+?>
