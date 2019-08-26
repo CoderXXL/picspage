7bf93e794631acb2f8ba3faa3f178587b3a2715d
dario <31698761+CoderXXL@users.noreply.github.com>
2019-08-26 13:11:17 +0200

    Comment out mail funtion

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $betreff = "Die Mail-Funktion";
 $from = "From: Vorname Nachname <absender@domain.de>";
 $text = "Hier lernt Ihr, wie man mit PHP Mails verschickt";
 
-mail($empfaenger, $betreff, $text, $from);
+# mail($empfaenger, $betreff, $text, $from);
 echo $empfaenger, $betreff, $text, $from;
 
 
